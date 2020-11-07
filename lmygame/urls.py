410836e51a872e3506e80051f34e5e92e04c88c9
yuta sakamoto <sorigeeeen@gmail.com>
2020-11-07 16:27:32 +0900

    add name_plate_list page template

--- lmygame/urls.py
+++ lmygame/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'lmygame'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/login', views.login, name='login'),
-    path('/selection', views.selection, name='selection'),
-    path('/name_plate_list', views.name_plate_list, name='name_plate_list'),
-    path('/question', views.question, name='question'),
-    path('/result', views.result, name='result'),
+    path('login/', views.login, name='login'),
+    path('selection/', views.selection, name='selection'),
+    path('name_plate_list/', views.name_plate_list, name='name_plate_list'),
+    path('question/', views.question, name='question'),
+    path('result/', views.result, name='result'),
 ]
